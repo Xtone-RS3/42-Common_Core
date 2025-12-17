8aa0007eccfa3b65574238c6cb817968c272e1f9
gasoares <gasoares@student.42porto.com>
2025-12-17 13:58:17 +0000

    more python

--- python 03/ex02/ft_coordinate_system.py
+++ python 03/ex02/ft_coordinate_system.py
@@ -5,14 +5,20 @@ if __name__ == "__main__":
     print("=== Game Coordinate System ===")
     print()
     if len(sys.argv) == 2:
-        point = tuple(sys.argv[1].split(sep=","))
+        try:
+            point = tuple(int(x) for x in sys.argv[1].split(sep=","))
+        except ValueError:
+            print(f"Error parsing coordinates: invalid literal for int() with base 10: '{sys.argv[1]}'")
+            sys.exit(0)
     elif len(sys.argv) == 4:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-        z = int(sys.argv[3])
-        print(f"Position created: ({x}, {y}, {z})")
-        point = tuple(sys.argv[1:])
-        dist = math.sqrt((0-x)**2 + (0-y)**2 + (0-z)**2)
-        print(f"Distance between (0, 0, 0) and ({x}, {y}, {z}): {dist:.2f}")
+        try:
+            point = tuple(int(x) for x in sys.argv[1:])
+        except ValueError:
+            print(f"Error parsing coordinates: invalid literal for int() with base 10: '{sys.argv[1]}'")
+            sys.exit(0)
     else:
         print("Error: Invalid coordinates")
+        sys.exit(0)
+    print(f"Position created: ({point[0]}, {point[1]}, {point[2]})")
+    dist = math.sqrt((0-point[0])**2 + (0-point[1])**2 + (0-point[2])**2)
+    print(f"Distance between (0, 0, 0) and ({point[0]}, {point[1]}, {point[2]}): {dist:.2f}")
